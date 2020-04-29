0d47b7ff66985c3d1ee658cf10989be0155e3bfb
Peoples5375 <58786672+Peoples5375@users.noreply.github.com>
2020-04-29 14:39:25 -0500

    First commit

---
+++ Migrations/20200428164908_initial3.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BeltExam.Migrations
+{
+    public partial class initial3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_DojoActivities_DojoActivities_DojoActivityId1",
+                table: "DojoActivities");
+
+            migrationBuilder.DropIndex(
+                name: "IX_DojoActivities_DojoActivityId1",
+                table: "DojoActivities");
+
+            migrationBuilder.DropColumn(
+                name: "DojoActivityId1",
+                table: "DojoActivities");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "DojoActivityId1",
+                table: "DojoActivities",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DojoActivities_DojoActivityId1",
+                table: "DojoActivities",
+                column: "DojoActivityId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_DojoActivities_DojoActivities_DojoActivityId1",
+                table: "DojoActivities",
+                column: "DojoActivityId1",
+                principalTable: "DojoActivities",
+                principalColumn: "DojoActivityId",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
